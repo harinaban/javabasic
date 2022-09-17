d16421c05c549faf48f808e0881d9f1d41c7421b
harinaban <harinaban@gmail.com>
2022-09-17 13:09:25 +0530

    first program

---
+++ javabase/test1.java
@@ -0,0 +1,18 @@
+package javabase;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java213");
+		int a=10,b=20;
+		String cr="r";
+		char c = 'a';
+		System.out.println(cr+a+b);
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println();
+	
+	}
+
+}
